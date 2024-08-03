a6872722cade089c0dde24f608449936583b1f2d
Leandro <leandro2791@hotmail.com>
2024-08-03 17:20:20 -0300

    Linker

---
+++ main.js
@@ -0,0 +1,11 @@
+function toggleMode(){
+    const body = document.body
+    const image = document.getElementById('img-logo')
+    body.classList.toggle('light')
+
+    if (body.classList.contains('light')){
+        image.setAttribute('src', '/assets/unknown2.png')
+    }else{
+        image.setAttribute('src', '/assets/unknown2.png') /* Pode trocar a foto aqui */
+    }
+}
